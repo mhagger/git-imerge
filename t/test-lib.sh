8c090eae440a4e6457fe5c95adc0825fbe6afdc2
Michael Haggerty <mhagger@alum.mit.edu>
2020-09-18 12:19:02 +0200

    create_test_repo, delete_test_repo: new test library functions

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -1,5 +1,44 @@
 # This file is meant to be sourced into other test scripts.
 
+init_test_repo() {
+    local path="$1"
+    local description="$2"
+
+    delete_test_repo "$path" "$description"
+
+    mkdir -p "$path"
+    git init "$path"
+    echo "$description" >"$path/.git/description"
+    printf '%s\n' "/*.out" "/*.css" "/*.html" >"$path/.git/info/exclude"
+    git --git-dir="$path/.git" config user.name 'Loú User'
+    git --git-dir="$path/.git" config user.email 'luser@example.com'
+
+    # Reset the timestamp that will be used for commits:
+    TIME=1112911993
+
+    # This CSS file is only for convenience, to make the HTML diagrams
+    # viewable:
+    ln -s "$BASE/imerge.css" "$path"
+}
+
+delete_test_repo() {
+    local path="$1"
+    local description="$2"
+
+    if test -d "$path"
+    then
+        # Be very careful before running "rm -rf":
+        if test "x$(cat "$path/.git/description")" = "x$description"
+        then
+            echo "Removing directory $path" >&2
+            rm -rf "$path"
+        else
+            echo "Directory $path doesn't look like our test repo!" >&2
+            exit 1
+        fi
+    fi
+}
+
 delete_branches() {
     for b in "$@"
     do
@@ -13,8 +52,6 @@ modify() {
     echo "$text" >"$filename" &&
     git add "$filename"
 }
-
-TIME=1112911993
 
 commit() {
     TIME=$(( TIME + 1 ))
