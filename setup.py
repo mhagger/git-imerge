cbf6e08b5f909f6207b0ee7691ad2004b9cc6da7
Jon Dufresne <jon.dufresne@gmail.com>
2020-11-28 05:19:58 -0800

    Add trove classifiers for Python 3.8 and 3.9
    Python 3.9 was released November 28, 2020.
    
    https://docs.python.org/dev/whatsnew/3.9.html

--- setup.py
+++ setup.py
@@ -45,6 +45,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Version Control :: Git",
