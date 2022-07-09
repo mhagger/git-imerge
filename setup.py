9aa23411261f511377dc497522d0173eecaf2038
Jon Dufresne <jon.dufresne@gmail.com>
2022-07-09 12:10:06 -0700

    Document support for Python 3.10

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Version Control :: Git",
