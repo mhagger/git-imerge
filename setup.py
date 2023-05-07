mhagger/git-imerge

commit cbf6e08b5f909f6207b0ee7691ad2004b9cc6da7

Add trove classifiers for Python 3.8 and 3.9
Python 3.9 was released November 28, 2020.

https://docs.python.org/dev/whatsnew/3.9.html

setup.py
@@ -45,6 +45,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Version Control :: Git",

commit 00889d72d197c59394599f4de09a96db9fcaef5b

Drop Python minor versions from programming language list
There will often be new minor versions and they mostly do not affect
git-imerge, so listing them all is an unnecessary maintenance burden.

Obsoletes: https://github.com/mhagger/git-imerge/pull/201
Fixes: commit cbf6e08b5f909f6207b0ee7691ad2004b9cc6da7
Fixes: commit 1c14b8a22f33a5ddeeee0811d91a6a63b1ab5e5e

setup.py
@@ -42,14 +42,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Environment :: Console",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Version Control :: Git",